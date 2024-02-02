2e7022a810b1487638f686cf4e77ba15f552ab90
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-02 16:47:11 -0500

    fix project issue

--- nextjs-course/nextjs-essentials/.next/server/server-reference-manifest.js
+++ nextjs-course/nextjs-essentials/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4QguLji14yOXYq+1s2Cz8WNHKu4ItDddx6I8l8l6uhI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"V276QmeDI71sbog6teNh3BloZEoMqM+owVx2Em++pDg=\"\n}"
