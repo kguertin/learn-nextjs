8692e00859d55a0a0eabb658ab0c681f09e6d290
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-11 15:57:55 -0500

    create app routing

---
+++ nextjs-course/foodies-app/app/meals/page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <main>
+      <h1>Meals Page</h1>
+      <p>
+        <Link href="/meals/share">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/featured-meal-1">Meal 1</Link>
+      </p>
+      <p>
+        <Link href="/meals/featured-meal-2">Meal 32==2</Link>
+      </p>
+    </main>
+  );
+}
