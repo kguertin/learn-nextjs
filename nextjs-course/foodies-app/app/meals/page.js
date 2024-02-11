5482c91c926cad88ba4708e2fc26d909538344ef
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-11 16:39:27 -0500

    create unstyled nav

--- nextjs-course/foodies-app/app/meals/page.js
+++ nextjs-course/foodies-app/app/meals/page.js
@@ -4,15 +4,6 @@ export default function MealsPage() {
   return (
     <main>
       <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/share">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/featured-meal-1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/featured-meal-2">Meal 32==2</Link>
-      </p>
     </main>
   );
 }
