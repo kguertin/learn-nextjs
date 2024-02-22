b5e960f241e507d607f92d1c7be144d0f62d6ba2
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-22 15:25:57 -0500

    create not found screen for meals

--- nextjs-course/foodies-app/app/not-found.js
+++ nextjs-course/foodies-app/app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Not Found</h1>
-      <p>Unfortunately, we could not find the requested page ot resource.</p>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
     </main>
   );
 }
