e144f81e43a2245ea66b36f1d36a38b291997d6f
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-26 10:34:04 -0500

    update page

--- next14-course/corp/src/app/performance/page.tsx
+++ next14-course/corp/src/app/performance/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/hero";
 import performanceImg from "../../../public/performance.jpg";
 
-export default function Performance() {
+export default function PerformancePage() {
   return (
     <Hero
       imgData={performanceImg}
