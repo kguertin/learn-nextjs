5bc09e293fae004b8ed68d149fc1d688903476ce
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-27 15:30:44 -0500

    create edit page

--- next14-course/snippets/src/app/snippets/[id]/page.tsx
+++ next14-course/snippets/src/app/snippets/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface SnippetShowPageProps {
@@ -22,7 +23,12 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
-          <button className="p-2 border rounded">Edit</button>
+          <Link
+            className="p-2 border rounded"
+            href={`/snippets/${snippet.id}/edit`}
+          >
+            Edit
+          </Link>
           <button className="p-2 border rounded">Delete</button>
         </div>
       </div>
