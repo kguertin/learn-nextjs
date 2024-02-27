5567d490ee06bab94df0ac5142d63c32bb95ee76
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-27 15:50:17 -0500

    add code editor and client component

--- next14-course/snippets/src/app/snippets/[id]/edit/page.tsx
+++ next14-course/snippets/src/app/snippets/[id]/edit/page.tsx
@@ -1,11 +1,25 @@
+import SnippetEditForm from "@/components/snippet-edit-form";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const id = parseInt(props.params.id);
 
-  return <div>Editing snippet with id {id}</div>;
+  const snippet = await db.snippet.findFirst({ where: { id } });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 }
---
+++ next14-course/snippets/src/components/snippet-edit-form.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+import { Editor } from "@monaco-editor/react";
+import { Snippet } from "@prisma/client";
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+}
