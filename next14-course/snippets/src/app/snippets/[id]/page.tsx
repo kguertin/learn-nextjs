9757a56b1327ebbd4eb9a32797797511f943c60b
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-28 15:50:44 -0500

    delete snippets

--- next14-course/snippets/src/app/snippets/[id]/page.tsx
+++ next14-course/snippets/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -18,6 +19,8 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
@@ -29,7 +32,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
             Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
