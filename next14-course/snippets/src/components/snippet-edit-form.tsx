11a9666f547c15af6797b629035298e97984a159
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-02-28 15:19:38 -0500

    use server action to save code updates

--- next14-course/snippets/src/components/snippet-edit-form.tsx
+++ next14-course/snippets/src/components/snippet-edit-form.tsx
@@ -1,16 +1,22 @@
 "use client";
+
 import { useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import { Snippet } from "@prisma/client";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   const [code, setCode] = useState(snippet.code);
+
   const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
       <Editor
@@ -21,6 +27,9 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button className="p-2 border rounded">Save</button>
+      </form>
     </div>
   );
 }
