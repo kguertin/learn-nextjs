02d7e9097e062cc77efbc8a0e0767aeceb8dc7c4
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-07 16:39:17 -0500

    build create post form

---
+++ next14-course/discuss/src/components/posts/postCreateForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useFormState } from "react-dom";
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import FormButton from "../common/formButton";
+
+export default function PostCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+            <Input
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+            <Textarea
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+            <FormButton>Create Post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
