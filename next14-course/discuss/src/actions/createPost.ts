1b2cd4cdd78206f60758b8eca39beda67097923d
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-08 10:26:44 -0500

    handle form validation for the post submission

--- next14-course/discuss/src/actions/createPost.ts
+++ next14-course/discuss/src/actions/createPost.ts
@@ -1,5 +1,45 @@
 "use server";
 
-export async function createPost() {
+import { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import paths from "@/paths";
+import { db } from "@/db";
+
+const createPostSchema = z.object({
+  title: z.string().min(3, { message: "Title must be 3 characters or longer" }),
+  content: z.string().min(10, {
+    message: "Content of the most must be a minimum of 10 characters long",
+  }),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
   //TODO: Revalidate the topic show page
 }
